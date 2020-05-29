c081d30d181153c1b89e9cecb403f47bee35b5bc
lwh <2431416414@qq.com>
2020-05-29 11:55:36 +0800

    登录界面动态添加bing每日壁纸 修复

--- src/utils/setBackGround.js
+++ src/utils/setBackGround.js
@@ -2,11 +2,11 @@ const request = require('superagent')
 
 export default async function(vm) {
   try {
-    const url = 'https://bird.ioliu.cn/v1?url=http://www.bing.com/HPImageArchive.aspx?format=js&idx=1&n=1'
+    const url = 'https://bird.ioliu.cn/v1?url=http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
     const result = await request.get(url)
     const resObj = JSON.parse(result.text)
     const imageURL = 'http://cn.bing.com' + resObj.images[0].url
-    const stlyString = "background-image:url('" + imageURL + "'); background-repeat:no-repeat; background-size:100% 100%;-moz-background-size:100% 100%;"
+    const stlyString = "background-image:url('" + imageURL + "'); background-repeat:no-repeat ;background-size:cover;"
     console.log(stlyString)
     vm.backGroundStyle = stlyString
     // vm.getbackGroundUrl =  'http://cn.bing.com'+resObj.images[0].url
